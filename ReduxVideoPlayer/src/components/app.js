b2045903ee71f1574d967f3149aa6e611a2d75be
saibot777 <stefantrajkovic777@gmail.com>
2017-11-09 03:22:57 +0100

    rendering videos...

--- ReduxVideoPlayer/src/components/app.js
+++ ReduxVideoPlayer/src/components/app.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './search_bar';
+import VideoList from './video_list';
+
+
+const API_KEY = 'AIzaSyAPGo-P9pW3EyELGbSN1iMfYyNHoSnCmR0';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
     );
   }
 }

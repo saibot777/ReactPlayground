686275ce86e8776e337242cbf060dc4b97f414ec
saibot777 <stefantrajkovic777@gmail.com>
2017-11-09 23:01:17 +0100

    merged redux with react, rendering book list redux state style

---
+++ ReduxBookStoreApp/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default () => {
+    return [
+        { title: 'Javascript: The Good Parts' },
+        { title: 'harry Potter' },
+        { title: 'Dark Tower' },
+        { title: 'Witcher' }
+    ]
+}

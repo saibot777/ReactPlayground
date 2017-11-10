16e9f46681986528d659e31d521b05add38e75f5
saibot777 <stefantrajkovic777@gmail.com>
2017-11-10 23:30:05 +0100

    general refactoring, showing post detail implemented...

---
+++ ReduxBlogApp/src/components/posts_show.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
+
+class PostsShow extends Component {
+    componentWillMount() {
+        this.props.fetchPost(this.props.params.id)
+    }
+
+    render() {
+        const { post } = this.props;
+
+        if(!this.props.post) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div>
+                <h2>{post.title}</h2>
+                <h4>Categories: {post.categories}</h4>
+                <p>{post.content}</p>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);

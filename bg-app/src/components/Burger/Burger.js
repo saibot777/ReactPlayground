2cf883e045b292082cfb88a37a5bd637b8f01590
saibot777 <stefantrajkovic777@gmail.com>
2017-12-14 13:12:20 +0100

    implemented default state in igredients array

--- bg-app/src/components/Burger/Burger.js
+++ bg-app/src/components/Burger/Burger.js
@@ -2,12 +2,18 @@ import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const trasnformedIngredients = Object.keys(props.ingredients)
+    let trasnformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (trasnformedIngredients.length === 0) {
+        trasnformedIngredients = <p>Please start adding ingredients</p>
+    }
     return (
         <div className="Burger">
              <BurgerIngredient type="bread-top" />

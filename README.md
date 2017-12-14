db7af8f6148b20ac603186937a2601027e3031a9
saibot777 <stefantrajkovic777@gmail.com>
2017-12-14 13:04:29 +0100

    added transforming ingredients func

--- README.md
+++ README.md
@@ -23,3 +23,11 @@ Checkout this repo, install dependencies, then start the webpack process with th
 
 > npm install
 > npm start
+
+## BG App - Tech used: React, Redux...
+
+### Install
+Checkout this repo, install dependencies, then start the webpack process with the following:
+
+> npm install
+> npm start
--- bg-app/src/components/Burger/Burger.js
+++ bg-app/src/components/Burger/Burger.js
@@ -2,11 +2,16 @@ import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const trasnformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        });
     return (
         <div className="Burger">
              <BurgerIngredient type="bread-top" />
-             <BurgerIngredient type="cheese" />
-             <BurgerIngredient type="meat" />
+             {trasnformedIngredients}
              <BurgerIngredient type="bread-bottom" />
         </div>
     );

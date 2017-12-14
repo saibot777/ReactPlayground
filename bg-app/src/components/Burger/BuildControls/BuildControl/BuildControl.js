c28227e1cffa2e564fffe20e8ba21c90cf70b0c7
saibot777 <stefantrajkovic777@gmail.com>
2017-12-14 15:23:07 +0100

    implemented remove and disable on 0 ingredients

--- bg-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ bg-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,8 +3,12 @@ import React from 'react';
 const buildControl = (props) => (
     <div className="BuildControl">
         <div className="Label">{props.label}</div>
-        <button className="Less">Less</button>
-        <button className="More" onClick={props.added}>More</button>
+        <button className="Less" 
+            onClick={props.removed} 
+            disabled={props.disabled}>Less</button>
+        <button 
+            className="More" 
+            onClick={props.added}>More</button>
     </div>
 );
 

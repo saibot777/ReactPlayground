fd2c8fc19c3e20135fcfeeda7ab1320d2a6acbf9
saibot777 <stefantrajkovic777@gmail.com>
2017-12-15 10:46:40 +0100

    navigation UI Toolbar done

--- bg-app/src/components/Layout/Layout.js
+++ bg-app/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../components/shared/Auxiliary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = ( props ) => {
     return (
         <Aux>
-            <div>Toolbar, SideDrawer, Backdrop</div>
+            <Toolbar />>
             <main className="Content">
                 {props.children}
             </main>

012819a689b1c2541ee640223f1d6071d9d58d12
saibot777 <stefantrajkovic777@gmail.com>
2017-12-24 00:00:24 +0100

    routing to checkout

--- bg-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ bg-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,28 +91,29 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Stefan Trajkovic',
-                address: {
-                    street: 'Juzni Bulevar',
-                    zipCode: '11000',
-                    country: 'Serbia'
-                },
-                email: 'test@mail.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Stefan Trajkovic',
+        //         address: {
+        //             street: 'Juzni Bulevar',
+        //             zipCode: '11000',
+        //             country: 'Serbia'
+        //         },
+        //         email: 'test@mail.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false});
+        //     });
+        this.props.history.push('/checkout');
 
     }
 

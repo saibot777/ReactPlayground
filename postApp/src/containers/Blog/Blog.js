6f7142ea7787ffd568ca2c919fd2e0afa9d4600e
saibot777 <stefantrajkovic777@gmail.com>
2017-12-23 16:31:35 +0100

    added Switch to navigate properly

--- postApp/src/containers/Blog/Blog.js
+++ postApp/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -19,8 +19,10 @@ class Blog extends Component {
                     </nav> 
                 </header>
                 <Route path="/" exact component={Posts} />
-                <Route path="/new-post" exact component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }

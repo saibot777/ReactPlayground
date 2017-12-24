2c7746aa8c604912a4f8011598345adfcd76fc6e
saibot777 <stefantrajkovic777@gmail.com>
2017-12-24 21:41:50 +0100

    order component implemented, passing data, redirection added also

--- bg-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ bg-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,32 +91,11 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Stefan Trajkovic',
-        //         address: {
-        //             street: 'Juzni Bulevar',
-        //             zipCode: '11000',
-        //             country: 'Serbia'
-        //         },
-        //         email: 'test@mail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({loading: false, purchasing: false});
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading: false, purchasing: false});
-        //     });
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',

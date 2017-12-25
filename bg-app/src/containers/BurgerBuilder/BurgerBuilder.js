7fe6e3ff88fa829a8ca33b84ff10405659467396
saibot777 <stefantrajkovic777@gmail.com>
2017-12-25 15:42:20 +0100

    redux initiated...

--- bg-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ bg-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,13 +31,13 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount () {
-        axios.get('https://bg-react-app.firebaseio.com/ingredients.json')
-            .then(response => {
-                this.setState({ingredients: response.data})
-            })
-            .catch(error => {
-                this.setState({error: true});
-            })
+        // axios.get('https://bg-react-app.firebaseio.com/ingredients.json')
+        //     .then(response => {
+        //         this.setState({ingredients: response.data})
+        //     })
+        //     .catch(error => {
+        //         this.setState({error: true});
+        //     })
     }
 
     updatePurchaseState (ingredients) {

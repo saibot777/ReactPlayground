97806dfdabdaca12e9620b0bc4d76e7c138e992e
saibot777 <stefantrajkovic777@gmail.com>
2017-12-25 15:06:55 +0100

    reducer refactored to use two reducers

--- counterApp/src/containers/Counter/Counter.js
+++ counterApp/src/containers/Counter/Counter.js
@@ -15,7 +15,7 @@ class Counter extends Component {
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubstractCounter}  />
                 <hr />
-                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {
                         this.props.storedResults.map(strResult =>(
@@ -30,8 +30,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter,
-        storedResults: state.results
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
     };
 };
 
@@ -41,7 +41,7 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
         onAddCounter: () => dispatch({type: actionTypes.ADD, val: 5 }),
         onSubstractCounter: () => dispatch({type: actionTypes.SUBSTRACT, val: 5}),
-        onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+        onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
         onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultID: id})
     };
 };

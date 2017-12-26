43d877b625dbd3f1e8be36f308d54441b9bc83d9
saibot777 <stefantrajkovic777@gmail.com>
2017-12-26 10:03:15 +0100

    refactored contact-data and checkout comps to use redux...

--- bg-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ bg-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -52,17 +52,8 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        const queryParams = [];
-        for (let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-        const queryString = queryParams.join('&');
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        });
-
+        // Store
+        this.props.history.push('/checkout');
     }
 
     render () {

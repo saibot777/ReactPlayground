f5219c17ef9276e530723acde3d41baccf76139d
saibot777 <stefantrajkovic777@gmail.com>
2017-12-27 13:43:04 +0100

    sign in action done

--- bg-app/src/store/actions/auth.action.js
+++ bg-app/src/store/actions/auth.action.js
@@ -1,4 +1,8 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
+
+const authURL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA9FY682WPQCaCMl-53I8b2DJlPoJI0oew';
 
 export const authStart = () => {
     return {
@@ -23,5 +27,19 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post(authURL, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
     };
 };

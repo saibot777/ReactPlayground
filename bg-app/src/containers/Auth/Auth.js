cd05425ba10b05bdcd3d9a04f77b9c414fc6a77a
saibot777 <stefantrajkovic777@gmail.com>
2017-12-28 10:02:45 +0100

    redirect on signin

--- bg-app/src/containers/Auth/Auth.js
+++ bg-app/src/containers/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import * as actions from '../../store/actions/index';
 import Input from '../../components/UI/Input/Input';
@@ -131,8 +132,14 @@ class Auth extends Component {
             ); 
         }
 
+        let authRedirect = null;
+        if (this.props.isAuthenticated) {
+           authRedirect = <Redirect to="/"/>
+        }
+
         return (
             <div className={classes.Auth}>
+                {authRedirect}
                 {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
@@ -151,7 +158,8 @@ class Auth extends Component {
 const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
-        error: state.auth.error
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null
     }
 };
 

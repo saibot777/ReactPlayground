55539fbe03e3d48db4811e2fba0372b0596290b3
saibot777 <stefantrajkovic777@gmail.com>
2017-12-28 15:03:33 +0100

    added env check for redux tools

--- bg-app/src/index.js
+++ bg-app/src/index.js
@@ -11,7 +11,10 @@ import burgerBuilderReducer from './store/reducers/burger.reducer';
 import orderReducer from './store/reducers/order.reducer';
 import authReducer from './store/reducers/auth.reducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' 
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ 
+    : null
+    || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,

a18a55ebbd9f0196741de0a34196db66d4d52e38
saibot777 <stefantrajkovic777@gmail.com>
2017-12-28 13:42:24 +0100

    bug fix for app.js

--- bg-app/src/App.js
+++ bg-app/src/App.js
@@ -60,4 +60,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));

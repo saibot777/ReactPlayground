b4e59e8f20c76f94014bcac12337fec3b8227179
saibot777 <stefantrajkovic777@gmail.com>
2018-01-16 16:53:57 +0100

    reducer created

---
+++ react-redux-example/src/modules/ui/reducers.js
@@ -0,0 +1,39 @@
+import { handleAction, combineActions } from 'redux-actions';
+
+import {
+    RECEIVE_ADD_ITEM,
+    receiveAddItem
+} from './actions';
+
+const defaultState = {
+    items: []
+};
+
+const receiveItems = (state, action) => {
+    let newState = {
+        ...state
+    };
+
+    newState.items = [].concat(newState.items);
+
+    if (Array.isArray(action.payload)) {
+        newState.items = newState.items.concat(action.payload);
+    } else {
+        newState.items.push(action.payload);
+    }
+}
+
+export const ui = handleAction(
+    combineActions({
+        receiveAddItem
+    }),
+    {
+        next(state, action) {
+            switch (action.type) {
+                case RECEIVE_ADD_ITEM: return receiveItems(state, action);
+                default:
+                    return state;
+            }
+        }
+    }
+)

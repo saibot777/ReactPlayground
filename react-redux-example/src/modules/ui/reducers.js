3507a7797991ebede0eb917bf9b0b34e2a606076
saibot777 <stefantrajkovic777@gmail.com>
2018-01-17 14:59:31 +0100

    fixes for reducer and redux-tools

--- react-redux-example/src/modules/ui/reducers.js
+++ react-redux-example/src/modules/ui/reducers.js
@@ -5,48 +5,43 @@ import {
     receiveAddItem
 } from './actions';
 
-const defaultState = {
+var defaultState = {
     items: []
 };
 
-const receiveItems = (state, action) => {
-    let newState = {
-        ...state
-    };
-    newState.items = [].concat(newState.items);
-    if (Array.isArray(action.payload)) {
-        newState.items = newState.items.concat(action.payload);
-    } else {
-        newState.items.push(action.payload);
-    }
-}
-
-const requestItems = (state, action) => {
-    let newState = {
-        ...state
-    };
-    newState.itemsError = action.payload;
-    return newState;
-}
-
 export const ui = handleAction(
-    combineActions({
+    combineActions(
         receiveAddItem
-    }),
+    ),
     {
         next(state, action) {
             switch (action.type) {
-                case RECEIVE_ADD_ITEM: return receiveItems(state, action);
+                case RECEIVE_ADD_ITEM: 
+                    var newState = {
+                        ...state
+                    };
+                    newState.items = [].concat(newState.items);
+                    if (Array.isArray(action.payload)) {
+                        newState.items = newState.items.concat(action.payload);
+                    } else {
+                        newState.items.push(action.payload);
+                    }
+                    return newState;
                 default:
                     return state;
             }
         },
         throw(state, action) {
            switch (action.type) {
-               case RECEIVE_ADD_ITEM: return requestItems(state, action);
+                case RECEIVE_ADD_ITEM: 
+                    var newState = {
+                    ...state
+                    };
+                    newState.itemsError = action.payload;
+                    return newState;
                default:
                     return state;
            } 
         }
-    }
+    }, defaultState
 )
--- react-redux-example/src/store.js
+++ react-redux-example/src/store.js
@@ -7,7 +7,13 @@ const sagaMiddleware = createSagaMiddleware();
 
 let middleware = applyMiddleware(sagaMiddleware);
 
-let composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+
+
+let composeEnhancers = typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
 
 export default createStore( rootReducer, {}, composeEnhancers(middleware));
 

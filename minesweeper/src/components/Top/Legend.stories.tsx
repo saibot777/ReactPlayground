abd708e64f3436f1839940ba5b50bc21314eb1f0
saibot777 <stefantrajkovic777@gmail.com>
2022-11-02 21:07:47 +0100

    Add Game Name comp

--- minesweeper/src/components/Top/Legend.stories.tsx
+++ minesweeper/src/components/Top/Legend.stories.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
-import { Legend } from "./Legend";
+import { Legend, LegendProps } from "./Legend";
 
 export default {
     title: 'Top/Legend',
     component: Legend
 } as Meta
 
-const Template: Story = (args) => {
+const Template: Story<LegendProps> = (args) => {
     return <Legend {...args} />;
 }
 
 export const GameLegend = Template.bind({})
+GameLegend.args = {
+    feature: 'Flag',
+    firstAction: 'ctrl',
+    secondAction: 'click'
+}

2ff6031ef39e4a43e5c91c1c96a6f8f87ed62162
saibot777 <stefantrajkovic777@gmail.com>
2022-11-02 20:36:10 +0100

    Add top comp

--- minesweeper/src/components/Top/Legend.stories.tsx
+++ minesweeper/src/components/Top/Legend.stories.tsx
@@ -3,7 +3,7 @@ import { Story, Meta } from '@storybook/react';
 import { Legend } from "./Legend";
 
 export default {
-    title: 'Legend',
+    title: 'Top/Legend',
     component: Legend
 } as Meta
 

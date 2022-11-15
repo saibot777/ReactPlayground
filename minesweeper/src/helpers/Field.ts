2f546bcaea1d68e894df447d27f4ead71b722667
saibot777 <stefantrajkovic777@gmail.com>
2022-11-15 01:42:49 +0100

    Add hooks

--- minesweeper/src/helpers/Field.ts
+++ minesweeper/src/helpers/Field.ts
@@ -8,15 +8,47 @@ export const CellState: Record<string, Cell> = {
   empty: 0,
   bomb: 9,
   hidden: 10,
-  mark: 11,
-  weakMark: 12,
+  flag: 11,
+  weakFlag: 12,
 };
 
+/**
+ * Create empty field
+ *
+ * Example emptyFieldGenerator(2)
+ * [
+ *   [0, 0],
+ *   [0, 0],
+ * ]
+ *
+ * @param {number} size
+ * @param {Cell} state
+ * @returns {Field}
+ */
 export const emptyFieldGenerator = (
   size: number,
   state: Cell = CellState.empty
 ): Field => new Array(size).fill(null).map(() => new Array(size).fill(state));
 
+/**
+ * Generate field with mines
+ * Cell has a range between 0 and 9 where
+ * 0 - have no any mine
+ * from 0 to 8 - count of mines arrount the cell
+ * 9 - special flag for the mine
+ *
+ * (1/9 ~ 11.11%)
+ * Example fieldGenerator(3, 1/9) would return
+ * [
+ *   [1, 1, 1],
+ *   [1, 9, 1],
+ *   [1, 1, 1]
+ * ]
+ *
+ * @param {number} size
+ * @param {number} probability
+ * @returns {Field}
+ */
 export const fieldGenerator = (size: number, probability: number): Field => {
   if (probability < 0 || probability > 1) {
     throw new Error("Probability must be between 0 and 1");
@@ -40,6 +72,5 @@ export const fieldGenerator = (size: number, probability: number): Field => {
       unprocessedCells--;
     }
   }
-
   return result;
 };

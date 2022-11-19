d93b2eb8dcebaf22734573528103bc9626f975d4
saibot777 <stefantrajkovic777@gmail.com>
2022-11-19 22:54:20 +0100

    Add useGame custom hook

--- minesweeper/src/modules/GameWithHooks/GameWithHooks.tsx
+++ minesweeper/src/modules/GameWithHooks/GameWithHooks.tsx
@@ -1,59 +1,26 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 
-import {
-  Field,
-  CellState,
-  generateFieldWithDefaultState,
-  fieldGenerator,
-  Coords,
-} from '@/helpers/Field';
-import { openCell } from '@/helpers/CellsManipulator';
-
-import { GameLevels, LevelNames, GameSettings } from '@/modules/GameSettings';
+import { GameLevels, LevelNames } from '@/modules/GameSettings';
 
 import { Top } from '@/components/Top';
 import { Scoreboard } from '@/components/Scoreboard';
 import { Grid } from '@/components/Grid';
 import { GameArea, Wrapper, GameOver } from '@/components/Game';
+import { useGame } from './useGame';
 
 export const GameWithHooks: FC = () => {
-  const [level, setLevel] = useState<LevelNames>('beginner');
-
-  const [size, bombs] = GameSettings[level];
-
-  const [playerField, setPlayerField] = useState<Field>(
-    generateFieldWithDefaultState(size, CellState.hidden)
-  );
-
-  const [gameField, setGameField] = useState<Field>(
-    fieldGenerator(size, bombs / (size * size))
-  );
-
-  const onClick = (coords: Coords) => {
-    const newPlayerField = openCell(coords, playerField, gameField);
-    setPlayerField([...newPlayerField]);
-  };
-
-  const resetHandler = ([size, bombs]: [number, number]) => {
-    const newGameField = fieldGenerator(size, bombs / (size * size));
-    const newPlayerField = generateFieldWithDefaultState(
-      size,
-      CellState.hidden
-    );
-
-    setGameField([...newGameField]);
-    setPlayerField([...newPlayerField]);
-  };
-
-  const onChangeLevel = ({
-    target: { value: level },
-  }: React.ChangeEvent<HTMLSelectElement>) => {
-    setLevel(level as LevelNames);
-    const newSettings = GameSettings[level as LevelNames];
-    resetHandler(newSettings);
-  };
-
-  const onReset = () => resetHandler([size, bombs]);
+    const {
+        level,
+        isGameOver,
+        isWin,
+        settings,
+        playerField,
+        onClick,
+        onChangeLevel,
+        onReset,
+      } = useGame();
+    
+    const [, bombs] = settings;
 
   return (
     <Wrapper>
@@ -66,10 +33,14 @@ export const GameWithHooks: FC = () => {
           bombs="10"
           levels={GameLevels as unknown as string[]}
           defaultLevel={level}
-          onChangeLevel={onChangeLevel}
+          onChangeLevel={({
+            target: { value: level },
+          }: React.ChangeEvent<HTMLSelectElement>) =>
+            onChangeLevel(level as LevelNames)
+          }
           onReset={onReset}
         />
-        <GameOver onClick={() => null} isWin={true} />
+        {isGameOver && <GameOver onClick={onReset} isWin={isWin} />}
         <Grid onClick={onClick} onContextMenu={() => null}>
           {playerField}
         </Grid>

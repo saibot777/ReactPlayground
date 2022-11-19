54ddf4b9003130ee70faf364e428b9a551f66987
saibot777 <stefantrajkovic777@gmail.com>
2022-11-19 21:49:36 +0100

    Add open cell func

--- minesweeper/src/helpers/CellsManipulator.ts
+++ minesweeper/src/helpers/CellsManipulator.ts
@@ -1,5 +1,10 @@
-import { Cell, Coords, Field } from "./Field";
+import { Cell, CellState, Coords, Field } from "./Field";
 
+/**
+ * Get neighbour cells indexes
+ * @param {Coords} coords
+ * @returns {Record<string, [number, number]>}
+ */
 export const getNeigboursItems = ([y, x]: Coords): Record<
   string,
   [number, number]
@@ -14,15 +19,26 @@ export const getNeigboursItems = ([y, x]: Coords): Record<
   leftTop: [y - 1, x - 1],
 });
 
+/**
+ * Check item in the field
+ * @param {Coords} coords
+ * @param {Field} field
+ * @returns {boolean}
+ */
 export const checkItemInField = ([y, x]: Coords, { length }: Field): boolean =>
   y >= 0 && x >= 0 && length - y > 0 && length - x > 0;
 
+/**
+ * Increment neighbour items for cell with coords
+ * @param {Coords} coords
+ * @param {Field} field
+ * @returns {Cell}
+ */
 export const incrementNeibours = (coords: Coords, field: Field): Field => {
   const items = getNeigboursItems(coords);
 
-  for (const item of Object.values(items)) {
-    if (checkItemInField(item, field)) {
-      const [y, x] = item;
+  for (const [y, x] of Object.values(items)) {
+    if (checkItemInField([y, x], field)) {
       const cell = field[y][x];
       if (cell < 8) {
         field[y][x] = (cell + 1) as Cell;
@@ -31,4 +47,51 @@ export const incrementNeibours = (coords: Coords, field: Field): Field => {
   }
 
   return field;
+};
+
+/**
+ * Open cell in the player field using game field info
+ * @param {Coords} coords
+ * @param {Field} playerField
+ * @param {Field} gameField
+ * @returns {Field}
+ */
+export const openCell = (
+  coords: Coords,
+  playerField: Field,
+  gameField: Field
+): Field => {
+  const { empty, hidden, bomb } = CellState;
+
+  const [y, x] = coords;
+  const gameCell = gameField[y][x];
+
+  if (gameCell === bomb) {
+    throw new Error("Game Over");
+  }
+
+  if (gameCell === empty) {
+    playerField[y][x] = gameCell;
+
+    const items = getNeigboursItems(coords);
+
+    for (const [y, x] of Object.values(items)) {
+      if (checkItemInField([y, x], gameField)) {
+        const gameCell = gameField[y][x];
+        const playerCell = playerField[y][x];
+
+        if (gameCell === empty && playerCell === hidden) {
+          playerField = openCell([y, x], playerField, gameField);
+        }
+
+        if (gameCell < bomb) {
+          playerField[y][x] = gameCell;
+        }
+      }
+    }
+  }
+
+  playerField[y][x] = gameCell;
+
+  return playerField;
 };

357c2cac499779080e6f9555b2c54ff54e0b6137
saibot777 <stefantrajkovic777@gmail.com>
2022-11-19 23:16:02 +0100

    Add openCell and setFlag methods

--- minesweeper/src/modules/GameWithHooks/GameWithHooks.tsx
+++ minesweeper/src/modules/GameWithHooks/GameWithHooks.tsx
@@ -6,21 +6,23 @@ import { Top } from '@/components/Top';
 import { Scoreboard } from '@/components/Scoreboard';
 import { Grid } from '@/components/Grid';
 import { GameArea, Wrapper, GameOver } from '@/components/Game';
+
 import { useGame } from './useGame';
 
 export const GameWithHooks: FC = () => {
-    const {
-        level,
-        isGameOver,
-        isWin,
-        settings,
-        playerField,
-        onClick,
-        onChangeLevel,
-        onReset,
-      } = useGame();
-    
-    const [, bombs] = settings;
+  const {
+    level,
+    isGameOver,
+    isWin,
+    settings,
+    playerField,
+    onClick,
+    onContextMenu,
+    onChangeLevel,
+    onReset,
+  } = useGame();
+
+  const [, bombs] = settings;
 
   return (
     <Wrapper>
@@ -30,7 +32,7 @@ export const GameWithHooks: FC = () => {
       <GameArea>
         <Scoreboard
           time="0"
-          bombs="10"
+          bombs={String(bombs)}
           levels={GameLevels as unknown as string[]}
           defaultLevel={level}
           onChangeLevel={({
@@ -41,7 +43,7 @@ export const GameWithHooks: FC = () => {
           onReset={onReset}
         />
         {isGameOver && <GameOver onClick={onReset} isWin={isWin} />}
-        <Grid onClick={onClick} onContextMenu={() => null}>
+        <Grid onClick={onClick} onContextMenu={onContextMenu}>
           {playerField}
         </Grid>
       </GameArea>
